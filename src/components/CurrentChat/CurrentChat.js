e5815d180b913610b131029f5642ae843e16650b
Siberian Johnny <allidon93@gmail.com>
2021-09-25 23:35:10 +0700

    .

--- src/components/CurrentChat/CurrentChat.js
+++ src/components/CurrentChat/CurrentChat.js
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import MessageList from '../MessageList/MessageList';
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
-import { addListItem, addMessage } from '../../actions/messagesActions';
+import { addMessage } from '../../actions/messagesActions';
 
 const CurrentChat = () => {
   const { chatId } = useParams();
 
-  const allMessages = useSelector((state) => state.messagesReducer.messageList)
+  const allMessages = useSelector((state) => state.messagesReducer.messageList);
   const dispatch = useDispatch();
 
   const onAddMessage = (message) => {
     dispatch(addMessage(chatId, message));
-  }
+  };
 
   useEffect(() => {
     const lastMessage = allMessages[chatId];
